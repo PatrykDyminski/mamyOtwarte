cd9e9656ea01a3ddefee426b5a76e09591cec6e0
Szymon <s.gasior.sw@wp.pl>
2021-03-18 11:49:14 +0100

    google connection test

--- pages/api/test.js
+++ pages/api/test.js
@@ -10,7 +10,7 @@ export default async function helloAPI(req, res) {
   const doc = new GoogleSpreadsheet('1AMPlXBjPoB_V0o6Va52reO24gDgn4qP5GgFr14aMD6Y');
   //await doc.useServiceAccountAuth(creds);
 
-  /*await doc.useServiceAccountAuth({
+  await doc.useServiceAccountAuth({
     ///client_email: creds.client_email,
     //private_key: creds.private_key,
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
@@ -22,6 +22,6 @@ export default async function helloAPI(req, res) {
   var newSheet = await doc.sheetsByIndex[1]
   var name;
   const rows = await newSheet.getRows();
-  name = rows[1].name;*/
-  res.status(200).json({ name: 'Test' })
+  name = rows[1].name;
+  res.status(200).json({ name: name })
 }

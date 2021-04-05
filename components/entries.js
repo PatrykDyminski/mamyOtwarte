4d50f1035347f95f4f011bfb859b668bae0f3b15
Patryk.Dyminski <patryk-dym@o2.pl>
2021-04-05 20:33:01 +0200

    changed returning entries

--- components/entries.js
+++ components/entries.js
@@ -2,7 +2,7 @@ export default function Entries({ entries }) {
   if (entries) {
     return (
       <div className="flex-grow">
-        {entries.places.map((e) => (
+        {entries.map((e) => (
           <div key={e.name} className="py-2 rounded bg-blue-400 mb-2 text-white">
             <h2>{e.name}-{e.type}</h2>
           </div>

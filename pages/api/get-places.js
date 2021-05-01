af64453c0a2d7cf2560f01eb30194e312c01ea4f
Szymon <s.gasior.sw@wp.pl>
2021-05-01 18:53:57 +0200

    added missing field to get-places

--- pages/api/get-places.js
+++ pages/api/get-places.js
@@ -4,7 +4,14 @@ function toJSONstr(place) {
   return ({
     "name": place.name,
     "type": place.type,
-    "opis": place.opis
+    "opis": place.opis,
+    "telephone": place.telephone,
+    "website": place.website,
+    "city": place.city,
+    "street": place.street,
+    "streetnr": place.streetnr,
+    "lat": place.lat,
+    "lng": place.lng
   });
 }
 

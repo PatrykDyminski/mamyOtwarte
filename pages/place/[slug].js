5eb964339ec6463298441792546f174582109a9f
Patryk.Dyminski <patryk-dym@o2.pl>
2021-05-06 18:48:47 +0200

    Styled Place page

--- pages/place/[slug].js
+++ pages/place/[slug].js
@@ -1,5 +1,7 @@
+import MapWrapper from '@/components/map-wrapper'
 import MyPage from '@/components/my-page'
 import getSheet from '@/lib/get-sheet'
+import { Marker } from '@react-google-maps/api';
 
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
@@ -9,7 +11,37 @@ export default function PlacePage({ place }) {
   return (
     <>
       <MyPage pageTitle={place.name}>
-        {place.name}
+        <div className="flex flex-col text-center items-center my-8">
+          <h1 className="text-6xl font-bold">
+            Nie do wiary! {place.name}
+            <div className="text-blue-600">
+              Mają Otwarte!
+            </div>
+          </h1>
+        </div>
+        
+        <div className="flex overflow-hidden h-4/6 gap-4">
+          <div className="w-1/3 px-2 py-4 bg-blue-500 rounded-md flex">
+            <ul className ="list-disc text-white text-2xl pt-4 pl-8">
+              <li>{place.type}</li>
+              <li>{place.opis}</li>
+              <li>{place.telephone}</li>
+              <li>{place.website}</li>
+              <li>{place.street} {place.streetnr}</li>
+              <li>{place.city}</li>
+            </ul>
+          </div>
+          <div className="w-2/3 px-2 py-4 bg-blue-400 text-center rounded-md">
+            <MapWrapper onMapClick={e => console.log(e.latLng.lat())}>
+              <Marker
+                position={{
+                  lat: parseFloat(place.lat),
+                  lng: parseFloat(place.lng),
+                }}
+              />
+            </MapWrapper>
+          </div>
+        </div>
       </MyPage>
     </>
   )

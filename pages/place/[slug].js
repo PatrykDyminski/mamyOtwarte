e73c3137f94a08a8b7d1f9432cb8bc70674b2d0e
Szymon <s.gasior.sw@wp.pl>
2021-05-06 17:03:54 +0200

    used lib function in fontend

--- pages/place/[slug].js
+++ pages/place/[slug].js
@@ -1,4 +1,5 @@
 import MyPage from '@/components/my-page'
+import getSheet from '@/lib/get-sheet'
 
 export default function PlacePage({ place }) {
   if (!place) return <></>
@@ -14,20 +15,25 @@ export default function PlacePage({ place }) {
 
 export async function getStaticProps({ params }) {
 
-  const res = await fetch(`http://localhost:3000/api/get-place/${params.slug}`)
-  const place = await res.json()
+  /*const res = await fetch(`http://localhost:3000/api/get-place/${params.slug}`)
+  const place = await res.json()*/
+
+  var query = { slug: params.slug };
+  const place = await getSheet(query, "ZweryfikowaneFiltr");
 
   return {
     props: {
-      place,
+      place: place[0],
     },
   }
 }
 
 export async function getStaticPaths() {
 
-  const res = await fetch('http://localhost:3000/api/get-places')
-  const places = await res.json()
+  //const res = await fetch('http://localhost:3000/api/get-places')
+  //const places = await res.json()
+  var query;
+  const places = await getSheet(query, "Zweryfikowane");
 
   const paths = places.map((place) => {
     return {

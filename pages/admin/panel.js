84e9e44c75f5adb1ad79545c795b60439592b1f0
Patryk.Dyminski <patryk-dym@o2.pl>
2021-06-03 14:30:53 +0200

    panel wip

--- pages/admin/panel.js
+++ pages/admin/panel.js
@@ -17,6 +17,8 @@ export default function Panel() {
     setActiveItem((prevState) => (prevState !== id ? id : ''));
   };
 
+  const { entries, isLoading } = useEntries()
+
   return (
     <MyPage pageTitle='Admin'>
       <div className="flex flex-col text-center items-center my-8">
@@ -25,69 +27,36 @@ export default function Panel() {
         </h1>
       </div>
       <div>
-        <AccordionHeader
-          id="item-1"
-          activeItem={activeItem}
-          onClick={toggleActiveItem('item-1')}
-        >
-          Accordion Group Item #1
-      </AccordionHeader>
-        <Accordion id="item-1" isOpen={activeItem}>
-          <p className="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-        </p>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-            ab illo inventore veritatis et quasi architecto beatae vitae dicta
-            sunt explicabo.
-        </p>
-        </Accordion>
-        <AccordionHeader
-          id="item-2"
-          activeItem={activeItem}
-          onClick={toggleActiveItem('item-2')}
-        >
-          Accordion Group Item #2
-      </AccordionHeader>
-        <Accordion id="item-2" isOpen={activeItem}>
-          <p className="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-            odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
-            quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
-            mauris. Fusce nec tellus sed augue semper porta.
-        </p>
-          <p>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-            et quas molestias excepturi sint occaecati cupiditate non provident,
-            similique sunt in culpa qui officia deserunt mollitia animi.
-        </p>
-        </Accordion>
-        <AccordionHeader
-          id="item-3"
-          activeItem={activeItem}
-          onClick={toggleActiveItem('item-3')}
-        >
-          Accordion Group Item #3
-      </AccordionHeader>
-        <Accordion id="item-3" isOpen={activeItem}>
-          <p className="mb-4">
-            dales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor.
-            Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas
-            mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas
-            porttitor. Morbi lectus risus, iaculis.
-        </p>
-          <p>
-            Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.
-            Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad
-            litora torquent per conubia nostra, per inceptos himenaeos. Nam nec
-            ante.
-        </p>
-        </Accordion>
+
+        {isLoading &&
+          <h1 className="text-white text-center text-2xl m-auto">Ładowanie...</h1>
+        }
+        <div className="flex-grow">
+          {!isLoading &&
+            entries.map((e) => (
+              <div key={e.name}>
+                <AccordionHeader
+                  id={e.slug}
+                  activeItem={activeItem}
+                  onClick={toggleActiveItem(e.slug)}
+                >
+                  <div className="">
+                    <h2 className="text-lg font-medium leading-relaxed">{e.name}</h2>
+                    <div className="flex flex-row justify-between">
+                      <h3 className="tracking-wide text-s title-font font-medium ">{e.type} - {e.city}</h3>
+                      <h3 className="text-s title-font font-medium ">{e.verified.toString()}</h3>
+                    </div>
+                  </div>
+                </AccordionHeader>
+                <Accordion id={e.slug} isOpen={activeItem}>
+                  <div className="bg-white rounded shadow-md">
+                    {e.city}
+                  </div>
+                </Accordion>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </MyPage>
   )

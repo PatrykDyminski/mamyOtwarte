b2963313ccc49b767dad5527b7f739df730a2951
Szymon <s.gasior.sw@wp.pl>
2021-06-03 12:50:24 +0200

    fixes after conflict

--- pages/api/get-all-places.js
+++ pages/api/get-all-places.js
@@ -1,9 +1,8 @@
 import getPlaces from '@/lib/get-places';
-import getDB from '@/lib/get-db';
+import db from '@/lib/db';
 
 export default async function getPlace(req, res) {
   if (req.method == 'GET') {
-    const db = getDB();
     var data = await getPlaces(req.query, true, true);
     res.status(200).json(data);
   }
